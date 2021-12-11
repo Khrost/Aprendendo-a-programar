1b7849c2f5057ad096262994da1249600b81acd8
Khrost Kont <francosprogrammermonx@gmail.com>
2021-12-11 15:46:21 -0300

    opcao de acrescimo - usando button

---
+++ scripts/redirecionamento.js
@@ -0,0 +1,15 @@
+let linkInicio = "../index.html";
+let linkSobre = "sobre.html";
+let linkProjetosUniversidade = "pdus.html";
+
+function btnPInicio(){
+    location.href = linkInicio;
+}
+
+function btnPSobre(){
+    location.href = linkSobre;
+}
+
+function btnPUniversidades(){
+    location.href = linkProjetosUniversidade;
+}
---
+++ scripts/redirecionamentoModIndex.js
@@ -0,0 +1,15 @@
+let linkInicio = "index.html";
+let linkSobre = "pages/sobre.html";
+let linkProjetosUniversidade = "pages/pdus.html";
+
+function btnPInicio(){
+    location.href = linkInicio;
+}
+
+function btnPSobre(){
+    location.href = linkSobre;
+}
+
+function btnPUniversidades(){
+    location.href = linkProjetosUniversidade;
+}
